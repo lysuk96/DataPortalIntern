68c989b5aa4ee1a3cb0002be3ff98c77e65efd53
zeroStone <lysuk96@hanyang.ac.kr>
2020-10-21 16:25:43 +0900

    First push

---
+++ 2_Replacing/2_replacing.py
@@ -0,0 +1,61 @@
+import openpyxl
+
+
+#엑셀 열기
+def open_xl():
+    global ws, wb
+
+    wb = openpyxl.load_workbook('1.xlsx')
+    ws = wb.active
+
+# 엑셀 저장 및 종료
+def save_exit_xl():
+    global wb
+
+    wb.save("1.xlsx")
+    wb.close()
+    exit()
+
+def chck_str(tmp) :
+    chck_arr = ["&lt;","&gt;","&#38;","&#x27;","&quot;","&#35;","&#40;","&#41;","&#x2F;"]
+    for i in range(0,9) :
+        if chck_arr[i] in tmp :
+            return True, chck_arr[i]
+    return False, None
+
+def chg_str(tmp) :
+    chckMap = {"&lt;":"<","&gt;":">","&#38;":"&","&#x27;":"\'","&quot;":"\"","&#35;":"#","&#40;":"(","&#41;":")","&#x2F;":"/"}
+    while (True):
+        row_limit, to_chg = chck_str(tmp)
+        if row_limit is False : break
+        tmp = tmp.replace(to_chg, chckMap[to_chg])
+    return tmp
+
+def fnd_row():
+    global row_limit, ws
+
+    chg_row = input("Which row do you want to change? : ")
+    chg_row = chg_row.upper()
+    
+    row_limit=1
+    while(row_limit<40) :
+        if(ws.cell(1,row_limit).value==None):
+            print("No such row in this sheet")
+            save_exit_xl()
+        elif(ws.cell(1,row_limit).value==chg_row):
+            break
+        row_limit=row_limit+1
+
+def chg_all():
+    global row_limit
+
+    for i in range(2, ws.max_row + 1) :
+        txt = chg_str(ws.cell(i,row_limit).value)
+        ws.cell(i,row_limit).value = txt
+
+
+# Main
+open_xl()
+fnd_row()
+chg_all()
+save_exit_xl()
---
+++ 2_Replacing/README.md
@@ -0,0 +1,35 @@
+# 엑셀 내 특정열 역치환
+----------------------
+
+## 홈페이지 오류 설명
+> 특수문자들이 치환된 형태로 불려와, 원문을 판단하기 어려움.
+> .xlsx 파일에 오류 목록을 들여, 역치환 알고리즘 적용하여 저장
+
+## 사용환경
+- Library : openpyxl
+- Tool : Visual Code
+- Language : Python
+
+## 개발환경 설정
+- python : 3.6.8
+- pip : 18.1
+- openpyxl : 3.0.5
+
+### -pip 환경변수 설정
+```
+1. 제어판 -> 시스템 및 보안 -> 시스템창
+2. 고급 시스템 설정 -> 환경변수
+3. pip path 경로 추가
+```
+
+### -openpyxl 설치
+```
+pip install openpyxl
+```
+
+## 사용법
+```
+1. 해당 xlsx파일을 '2_replacing.py'와 같은 dir에 포함시킨다.
+2. 콘솔창에 "python 2_replacing.py" 입력
+3. "Which row do you want to change? : " -> 역치환하려는 목록명 입력
+```
